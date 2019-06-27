ca0906ff08b901164f1ba78e50dc027a20132336
Dan Winga <winga@localhost.localdomain>
2019-06-27 14:42:15 +0300

    Updates

---
+++ precache-manifest.e711c84410684df0b59216ce761537c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b64233cbd38a33a73b36487ba03b957",
+    "url": "/danWinga/record-store/index.html"
+  },
+  {
+    "revision": "d3f0a478efbb8ec979a3",
+    "url": "/danWinga/record-store/static/css/2.9798096d.chunk.css"
+  },
+  {
+    "revision": "9c752e9d043bed102a97",
+    "url": "/danWinga/record-store/static/css/main.c22cd68a.chunk.css"
+  },
+  {
+    "revision": "d3f0a478efbb8ec979a3",
+    "url": "/danWinga/record-store/static/js/2.8ff48792.chunk.js"
+  },
+  {
+    "revision": "9c752e9d043bed102a97",
+    "url": "/danWinga/record-store/static/js/main.334fc315.chunk.js"
+  },
+  {
+    "revision": "f56828f5b8060581b92b",
+    "url": "/danWinga/record-store/static/js/runtime~main.ef5cf065.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/danWinga/record-store/static/media/logo.9f7e506d.svg"
+  }
+]);
